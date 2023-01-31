773d9c80abc9422a925b26ef73a346fc0bee79d4
Neetu Vishwakarma <124052986+Neetu20Karma@users.noreply.github.com>
2023-01-31 17:55:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+//Todo:Make M+ and m- and MC not working.....
+let string = "";
+
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+   button.addEventListener('click',(e)=>{
+      if(e.target.innerHTML == '='){
+         string = eval(string);
+         document.querySelector('input').value = string;
+      }
+     else if(e.target.innerHTML == 'c'){
+         string = ""
+         document.querySelector('input').value = string;
+      }
+      else{
+      console.log(e.target)
+      string = string + e.target.innerHTML;
+      document.querySelector('input').value = string;
+   }
+   })
+})
+      
+         
+         
+      
+    
